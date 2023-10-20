71b0fb0218d732a8e9efd5439fa963fe3983b254
Zelef <crazzzy0meat@gmail.com>
2023-10-20 22:16:00 +0600

    optimization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,9 @@
 package org.example;
 
 import org.example.Contoller.CalculatorController;
-import org.example.Logger.CalculatorLogger;
-import org.example.Model.Calculator;
-import org.example.Model.CalculatorLogic;
-import org.example.Model.ComplexCalculator;
-import org.example.View.CalculatorService;
-import org.example.View.CalculatorView;
 
 public class Main {
     public static void main(String[] args) {
-
-
         CalculatorController controller = new CalculatorController();
         controller.start();
     }
